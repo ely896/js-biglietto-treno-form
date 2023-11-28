cee7fbf7ede5287c2c7a9d1b7d1de56407417e5b
ely896 <saretta2011@libero.it>
2023-11-28 23:00:20 +0100

    Add milestone 2

--- main.js
+++ main.js
@@ -1,69 +1,82 @@
-/*Descrizione:
-Scrivere un programma che chieda all’utente:
-Il numero di chilometri da percorrere
-Età del passeggero Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
-il prezzo del biglietto è definito in base ai km (0.21 € al km)
-va applicato uno sconto del 20% per i minorenni
-va applicato uno sconto del 40% per gli over 65.
-MILESTONE 1:
-Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
+/*MILESTONE 2:
+Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un 
+form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo. 
+Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina 
+(il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
+ Questo richiederà un minimo di ricerca.
+Nota:
+Se non vi sentite particolarmente creativi, questa potrebbe essere un’implementazione da seguire per 
+la seconda milestone.
+Potete scegliere di implementare una soluzione completamente diversa oppure simile, 
+ma in ogni caso cercate di farla vostra.
 */
 
 
-//Dichiarazione variabile prezzo finale
+//Dichirazione variabile generazione ticket
 
-const generateBtnElement = document.getElementById('generate');
+const generateInputElement = document.getElementById('generate');
 
-//Dichiarazione variabile km da percorrere
+//dichiarazione variabile nome
+
+const nomeInputElement = document.getElementById('nome');
+
+//Dichiarazione variabile cognome
+
+const cognomeInputElement = document.getElementById('cognome');
+
+//Dichiarazione varibaile km
+
 const kmInputElement = document.getElementById('km');
 
+//Dichiarazione varibaile età
 
-//Dichiarazione variabile età
 const ageInputElement = document.getElementById('age');
 
-//Dichiarazione variabile prezzo al km
-const price_per_km = 0.21;
+//add evento in ascolto
 
-generateBtnElement.addEventListener('click', function () {
+generateInputElement.addEventListener('click', function() {
 
-    //console.log('clicked', ageInputElement.value, kmInputElement.value);
-
+    //console.log('clicked', nomeInputElement.value, cognomeInputElement.value, kmInputElement.value, ageInputElement.value);
     const km = parseInt(kmInputElement.value);
 
     const age = parseInt(ageInputElement.value);
 
-    const full_price = km * price_per_km;
+    //dichiarazione variabile costo al km
+
+    const price_km = 0.21;
 
     //Dichiarazione variabile sconto
+
     let discount;
 
+    //Dichiarazione prezzo del biglietto senza sconti
 
-    if (age < 18) {
-        console.log('apply a 20% discuount');
+    const fullPrice = km * 0.21
 
+    if (age <18) {
         discount = 0.2
 
     } else if (age > 65) {
-
-        console.log('apply a 40% discuount');
         discount = 0.4
-
     } else {
-        console.log('apply full price');
         discount = 0
     }
 
-    console.log(discount);
+    //Dichiarazione variabile Prezzo Finale
 
-    const final_price = full_price - (full_price * discount);
+    const finalPrice = fullPrice - (fullPrice * discount);
 
-    //Dichiarazione variabile prezzo finale
+    
+    console.log('Il prezzo del biglietto é di:', 'euro' , + finalPrice.toFixed(2));
 
-    console.log('Il prezzo del biglietto é:', 'euro' + final_price.toFixed(2));
-
-    document.getElementById('ticket').innerHTML = 'Il prezzo del biglietto é: ' + 'euro' + final_price.toFixed(2);
-
+    document.getElementById('ticket').innerHTML = 'Il prezzo del biglietto é: ' + 'euro' + finalPrice.toFixed(2);
+    document.getElementById('name').innerHTML = 'nome: ' + nomeInputElement.value;
+    document.getElementById('surname').innerHTML = 'cognome: ' + cognomeInputElement.value;
+    document.getElementById("carrozza").innerHTML = 'numero carrozza: ' + Math.floor(Math.random() * 10);
 })
+
+
+
 
 
 
